4b12dd0a89ed2a025c6b0024374ab5bf2671f2f1
Julian Beggs <julianbeggs@gmail.com>
2023-11-21 19:28:57 -0300

    entry page ok

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'encyclopedia'
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/", views.index, name="index"),
+    path("<str:title>", views.entry, name="entry"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("new/", views.new, name="new"),
+    path("edit/", views.edit, name="edit"),
+    path("random/", views.random, name="random"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
-
+import markdown2
+from django.http import request, response
 from . import util
 
 
 def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    if util.get_entry(title) != None:
+        entry = markdown2.markdown(util.get_entry(title))
+    else:
+        entry = None
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "entry": entry
+    })
+
+def new(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def edit(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def random(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
